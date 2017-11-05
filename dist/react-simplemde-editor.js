78fc2e28ec36c3ba389ccbc1a1b51aa7b500e67c
Sergio M <sergiomattei@outlook.com>
2017-11-05 01:55:38 -0400

    New build

--- dist/react-simplemde-editor.js
+++ dist/react-simplemde-editor.js
@@ -94,10 +94,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
 	    if (!this.state.keyChange && nextProps.value !== this.simplemde.value()) {
-	      console.log("Value changed");
+	      // Prevent a bad bug.
+	      console.log("SimpleMDE: Value changed");
 	      this.simplemde.value(nextProps.value);
 	      this.simplemde.codemirror.focus();
-	      // Set the cursor at the end of existing content
 	      this.simplemde.codemirror.setCursor(this.simplemde.codemirror.lineCount(), 0);
 	    }
 	
